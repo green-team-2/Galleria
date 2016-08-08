78fa05de00e41f66590202a860a5eed9f5b301ee
alessandro12p <bapolisena.list@gmail.com>
2016-08-08 15:18:32 +0200

    .

--- GalleriaJava/src/main/GalleriaJava.java
+++ GalleriaJava/src/main/GalleriaJava.java
@@ -2,6 +2,6 @@ package main;
 
 public class GalleriaJava {
     public static void main(String[] args) {
-        System.out.println("Green Team Reigns Supreme.");
+        System.out.println("Hello, world.");
     }
 }
